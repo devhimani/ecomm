2f87aab5d19731e9c0ee913c15ae0e0a907f94ab
himanigupta <gupta.himani44@gmail.com>
2024-12-12 23:19:52 -0800

    Integrate reusable components into SignIn page structure

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+// import { Loader2 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <form>
+        <Label htmlFor="email">Email</Label>
+        <Input />
+        <Label htmlFor="email">Password</Label>
+        <Input />
+        <Button disabled>
+          {/* <Loader2 className="animate-spin" />
+        Please wait */}
+          Sign up
+        </Button>
+      </form>
+      <div>
+        Dont have an account? <Link to="/signup">Signup</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
