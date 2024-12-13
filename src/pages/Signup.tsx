848583e59e23901fe0d423479f96289f21083141
himanigupta <gupta.himani44@gmail.com>
2024-12-13 02:06:59 -0800

    Integrate reusable components and style signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+// import { Loader2 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="border m-2 p-4 rounded-lg w-full max-w-sm bg-gray-100 flex flex-col gap-4">
+        <h1 className="text-center text-3xl">Sign Up</h1>
+        <form>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="name"
+            id="name"
+            placeholder="Name"
+            className="bg-white"
+          />
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            placeholder="Email"
+            className="bg-white"
+          />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            placeholder="Password"
+            className="bg-white"
+          />
+          <Button className="mt-2" disabled>
+            {/* <Loader2 className="animate-spin" />
+        Please wait */}
+            Sign Up
+          </Button>
+        </form>
+        <div className="text-sm">
+          Already have an acoount
+          <Link className="underline" to="/signin">
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
